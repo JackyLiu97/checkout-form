2846d47af727d2096f7a05c76ca588949d9e5cd9
Jacky Liu <jackyliu977@gmail.com>
2020-10-12 14:54:10 -0400

    fixed alignment with firstName lastName Username and Email

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,19 @@
-// calculator input
 
-const button = document.querySelectorAll(".btn btn-primary")
-button.onclick =  getVal
-//("click", getVal)
+// const button = document.querySelectorAll(".btn btn-primary")
+// button.onclick =  getVal
+// //("click", getVal)
 
-function getVal() {
-let val = document.querySelectorAll(".btn btn-primary").textContent
-let element = document.createElement("p");
-element.appendChild(document.createTextNode(val))
-document.getElementById("flex-bot-result").appendChild(element);
+// function getVal() {
+// let val = document.querySelectorAll(".btn btn-primary").textContent
+// let element = document.createElement("p");
+// element.appendChild(document.createTextNode(val))
+// document.getElementById("flex-bot-result").appendChild(element);
 
-}
-
+// }
 
 
 
-function clear(){
-    document.getElementById('flex-box-result').remove();
-}
+
+// function clear(){
+//     document.getElementById('flex-box-result').remove();
+// }
